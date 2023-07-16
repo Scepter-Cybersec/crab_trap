a18380917a7ac45c65fe56cf4e4acfec10bd8e65
wadeking98 <wkingnumber2@gmail.com>
2023-07-16 13:51:57 -0700

    delete fully working
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- src/menu/menu_list.rs
+++ src/menu/menu_list.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout, Stdout, Write};
+use termion::cursor::DetectCursorPos;
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::{color, cursor};
-use tokio::sync::{MutexGuard, mpsc};
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::{clear, color, cursor};
+use tokio::sync::{mpsc, MutexGuard};
 
 use crate::socket::connection;
 
@@ -28,10 +29,10 @@ pub fn help() {
 }
 
 pub fn clear() {
-    println!("{clear}", clear = termion::clear::All);
+    println!("{clear}", clear = clear::All);
 }
 
-fn start(key: String, connected_shells: HashMap<String, connection::Handle>){
+fn start(key: String, connected_shells: HashMap<String, connection::Handle>) {
     let handle = match connected_shells.get(&key) {
         Some(val) => val,
         None => {
@@ -44,102 +45,153 @@ fn start(key: String, connected_shells: HashMap<String, connection::Handle>){
     handle.egress.send("start").unwrap();
 }
 
-
-macro_rules! deselect_item {
-    ($stdout:expr,$idx:expr,$items:expr) => {
-        let (_, height) = termion::terminal_size().unwrap();
-        let cur_display_idx = height - ($items.len() - $idx - 1) as u16;
-        write!(
-            $stdout,
-            "{goto}{key}{reset}",
-            goto = cursor::Goto(0, cur_display_idx as u16),
-            key = *$items[$idx],
-            reset = color::Bg(color::Reset),
-        )
-        .unwrap();
-        $stdout.flush().unwrap();
+fn delete(key: String, connected_shells: &mut MutexGuard<HashMap<String, connection::Handle>>) {
+    let handle = match connected_shells.get(&key) {
+        Some(val) => val,
+        None => {
+            println!("Invalid session key!");
+            return;
+        }
+    };
+    //delete handler
+    handle.egress.send("delete").unwrap();
+    match connected_shells.remove(&key) {
+        Some(val) => val.soc_kill_token.cancel(),
+        None => return,
     };
 }
 
-macro_rules! select_item {
-    ($stdout:expr,$idx:expr,$items:expr) => {
-        let (_, height) = termion::terminal_size().unwrap();
-        let cur_display_idx = height - ($items.len() - $idx - 1) as u16;
-        write!(
-            $stdout,
-            "{goto}{select}{key}{reset}",
-            goto = cursor::Goto(0, cur_display_idx as u16),
-            select = color::Bg(color::LightRed),
-            key = *$items[$idx],
-            reset = color::Bg(color::Reset),
-        )
-        .unwrap();
-        $stdout.flush().unwrap();
+macro_rules! unlock_menu {
+    ($menu_channel_release:expr) => {
+        println!(
+            "\r\n{show}{blink}{clear}",
+            show = cursor::Show,
+            blink = cursor::BlinkingBlock,
+            clear = clear::AfterCursor
+        );
+        let menu_esc_release = $menu_channel_release.clone();
+        tokio::spawn(async move {
+            menu_esc_release.send(true).await.unwrap();
+            return;
+        });
     };
+}
+
+// fn list_menu_help(
+//     stdout: &mut RawTerminal<Stdout>,
+//     println!()
+// ){}
+
+fn refresh_list_display(
+    stdout: &mut RawTerminal<Stdout>,
+    start_pos: u16,
+    cur_idx: usize,
+    keys: Vec<&String>,
+) {
+    write!(
+        stdout,
+        "{goto}{clear}",
+        goto = cursor::Goto(0, start_pos),
+        clear = clear::AfterCursor
+    )
+    .unwrap();
+    stdout.flush().unwrap();
+    for (i, key) in keys.clone().into_iter().enumerate() {
+        let selection: String;
+        if i == cur_idx {
+            selection = format!(
+                "{select}{key}{reset}{hide}",
+                key = key,
+                select = color::Bg(color::LightRed),
+                hide = cursor::Hide,
+                reset = color::Bg(color::Reset),
+            );
+        } else {
+            selection = format!("{key}{hide}", key = key, hide = cursor::Hide,);
+        }
+        write!(stdout, "{}", selection).unwrap();
+        if i < &keys.len() - 1 {
+            write!(stdout, "\r\n{clear}", clear = clear::AfterCursor).unwrap();
+        }
+        stdout.flush().unwrap();
+    }
+    // select_item!(stdout, cur_idx, keys, true);
 }
 
 pub fn new() -> MenuList {
     let mut menu: MenuList = HashMap::new();
 
-    let list = |connected_shells: MutexGuard<HashMap<String, connection::Handle>>,
+    let list = |mut connected_shells: MutexGuard<HashMap<String, connection::Handle>>,
                 menu_channel_release: mpsc::Sender<bool>| {
-        let shells = connected_shells.clone();
         let stdin = stdin();
         let mut stdout = stdout().into_raw_mode().unwrap();
-        for (i, (key, _)) in shells.clone().into_iter().enumerate() {
-            write!(stdout, "{}{hide}", key, hide = cursor::Hide).unwrap();
-            if i < shells.len() - 1 {
-                write!(stdout, "\r\n").unwrap();
-            }
-            stdout.flush().unwrap();
-        }
-        if shells.len() > 0 {
+        if connected_shells.len() > 0 {
+            let (_, start_pos) = stdout.cursor_pos().unwrap();
             let mut cur_idx = 0;
-            let keys = shells.keys().collect::<Vec<&String>>();
-            select_item!(stdout, cur_idx, keys);
+            // refresh shell list
+            {
+                let keys = connected_shells.keys().collect::<Vec<&String>>();
+                refresh_list_display(&mut stdout, start_pos, cur_idx, keys);
+            }
+            let mut line_offset: i16 = 1;
             for key in stdin.keys() {
-                match key.unwrap() {
-                    Key::Esc => {
-                        println!(
-                            "\r\n{show}{blink}",
-                            show = cursor::Show,
-                            blink = cursor::BlinkingBlock
-                        );
-                        let menu_esc_release = menu_channel_release.clone();
-                        tokio::spawn(async move {
-                            menu_esc_release.clone().send(true).await.unwrap();
-                            return
-                        });
-                        return;
-                    }
-                    Key::Up => {
-                        if cur_idx > 0{
-                            deselect_item!(stdout, cur_idx, keys);
-                            cur_idx -= 1;
-                            select_item!(stdout, cur_idx, keys);
+                {
+                    match key.unwrap() {
+                        Key::Esc => {
+                            unlock_menu!(menu_channel_release);
+                            return;
                         }
-                    },
-                    Key::Down => {
-                        if cur_idx < keys.len() - 1{
-                            deselect_item!(stdout, cur_idx, keys);
-                            cur_idx += 1;
-                            select_item!(stdout, cur_idx, keys);
+                        Key::Up => {
+                            if cur_idx > 0 {
+                                cur_idx -= 1;
+                            }
                         }
-                    },
-                    Key::Char('\n') =>{
-                        let key = keys[cur_idx].into();
-                        start(key, shells.clone());
-                        println!(
-                            "\r\n{show}{blink}",
-                            show = cursor::Show,
-                            blink = cursor::BlinkingBlock
-                        );
-                        return;
+                        Key::Down => {
+                            if cur_idx < connected_shells.len() - 1 {
+                                cur_idx += 1;
+                            }
+                        }
+                        Key::Char('\n') | Key::Char('\r') => {
+                            let key = connected_shells.clone().keys().collect::<Vec<&String>>()
+                                [cur_idx]
+                                .into();
+                            start(key, connected_shells.clone());
+                            println!(
+                                "\r\n{show}{blink}",
+                                show = cursor::Show,
+                                blink = cursor::BlinkingBlock
+                            );
+                            return;
+                        }
+                        Key::Char('d') => {
+                            let key: String =
+                                connected_shells.clone().keys().collect::<Vec<&String>>()[cur_idx]
+                                    .into();
+                            delete(key.to_owned(), &mut connected_shells);
+                            line_offset -= 1;
+                            if cur_idx > 0 {
+                                cur_idx -= 1;
+                            }
+
+                            if connected_shells.is_empty() {
+                                unlock_menu!(menu_channel_release);
+                                return;
+                            }
+                        }
+                        _ => {}
                     }
-                    _ => {}
                 }
+
+                let shells = connected_shells.clone();
+                refresh_list_display(
+                    &mut stdout,
+                    ((start_pos as i16 + line_offset) - (shells.len() as i16)) as u16,
+                    cur_idx,
+                    connected_shells.clone().keys().collect::<Vec<&String>>(),
+                );
             }
         }
+        unlock_menu!(menu_channel_release);
     };
     menu.insert("l", Box::new(list));
 
